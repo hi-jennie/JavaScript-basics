7f485d9b4bf5a801c338be7bb3c564b6958c39a6
hi-jennie <jennie.wang2024@gmail.com>
2024-10-20 22:27:56 +0800

    inheritance

--- classes.js
+++ classes.js
@@ -12,3 +12,16 @@ const person = new Person('Jennie', 25);
 const person2 = new Person('Rustin', 28);
 
 console.log(person.name);
+
+
+class Teacher extends Person {
+    constructor(name, age, subject) {
+        super(name, age);
+        this.subject = subject;
+    }
+    teach(){
+        console.log('teach');
+    }
+}
+
+const teacher = new Teacher('Jennie', 25, 'CS');
