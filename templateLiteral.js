d7fd54507274564ccf768b9a85d73eaa718dc933
hi-jennie <jennie.wang2024@gmail.com>
2024-10-20 18:18:51 +0800

    template literal

---
+++ templateLiteral.js
@@ -0,0 +1,18 @@
+const message1 = 'This is my\n' +
+'\'first\'  message1';
+
+// template literal
+const message2 = `This is my
+'first' message2`;
+
+// ${name} is a placeholder for a variable or expression.
+const name = "Jennie";
+const email = 
+`Hi ${name},
+
+Thank you for joining my mailing list.
+
+Regards,
+Jennie`;
+
+console.log(email);
--- variable-scope.js
+++ variable-scope.js
@@ -14,6 +14,18 @@ function start() {
     console.log(i); // Error: i is not defined
 }
 
+// this keyword
+const video = {
+    title: 'a',
+    tags: ['a', 'b', 'c'],
+    showTags() {
+        this.tags.forEach(function(tag) {
+            console.log(this.title, tag); 
+        },this);
+    }
+};
+// if we don't pass the second argument to forEach, this.title will refer to the global object, which is undefined in strict mode.
 
-
-
+// 	1.	forEach 回调中的 this 默认不会继承外层作用域的 this，需要显式指定。
+// 	2.	forEach 的第二个参数 可以传递一个值作为回调函数的 this，确保 this 在回调中指向我们想要的对象。
+//	3.	如果不使用第二个参数，还有其他解决方案，比如箭头函数（箭头函数不会改变 this 的指向）。
